345329a770a576dbea79251e43932f737f38c258
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-12-26 18:12:56 +0000

    All api lab work completed again because of cloning issues

---
+++ movies-api/index.js
@@ -0,0 +1,53 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
+import './db';
+import './seedData';
+import usersRouter from './api/users';
+import passport from './authenticate';
+import session from 'express-session';
+import authenticate from './authenticate';
+
+dotenv.config();
+
+// eslint-disable-next-line no-unused-vars
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
+const app = express();
+
+// replace app.use(session([... with the following:
+app.use(passport.initialize());
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/genres', genresRouter);
+// eslint-disable-next-line no-irregular-whitespace
+// Add passport.authenticate(..)  to middleware stack for protected routes​
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use(errHandler);
+//session middleware
+app.use(session({
+    secret: 'ilikecake',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
+
+//update /api/Movie route
+app.use('/api/movies', authenticate, moviesRouter);
